d2f0a7092cf81b0b3f43088a774073a3ca39d6c2
zhaochonghuan <1561808630@qq.com>
2018-05-29 11:16:14 +0800

    app

--- js/yanzhengshoujihao.js
+++ js/yanzhengshoujihao.js
@@ -1,8 +1,6 @@
 function checkPhone(el){ //'#sdf'|| '.djf'
 	var phone = $(el).val();
-    if(!(/^1[34578]\d{9}$/.test(phone))){ 
-    	mui.toast('手机号错误');
-        $(el).focus();
+    if(!(/^1[34578]\d{9}$/.test(phone))){
         return false; 
     }else{
     	return true;
